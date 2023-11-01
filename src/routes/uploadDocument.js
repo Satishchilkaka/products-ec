ce88e01ea97c6294a70b014e06863838a159b427
Satish <73182406+Satishchilkaka@users.noreply.github.com>
2023-10-31 22:02:49 -0400

    add update and delete doc

--- src/routes/uploadDocument.js
+++ src/routes/uploadDocument.js
@@ -1,11 +1,12 @@
+
+
 const express = require("express");
-const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
+const { S3Client, PutObjectCommand, CopyObjectCommand, DeleteObjectCommand } = require("@aws-sdk/client-s3");
 const multer = require("multer");
 const multerS3 = require("multer-s3");
 const path = require("path");
 
 const app = express();
-
 
 const s3Client = new S3Client({
   region: process.env.AWS_REGION,
@@ -17,20 +18,18 @@ const s3Client = new S3Client({
 
 const s3Storage = multerS3({
   s3: s3Client,
-  bucket: process.env.AWS_BUCKET_NAME, 
+  bucket: process.env.AWS_BUCKET_NAME,
   metadata: function (req, file, cb) {
     cb(null, { fieldName: file.fieldname });
   },
   key: function (req, file, cb) {
-    const extname = path.extname(file.originalname);
-    const filename = `${file.fieldname}-${Date.now()}${extname}`;
-    cb(null, filename);
+    const originalname = file.originalname;
+    cb(null, originalname);
   },
 });
 
-
 const fileFilter = (req, file, cb) => {
-  const allowedExtensions = ['.pdf', '.jpeg', '.jpg', '.png', '.doc', '.docx, .txt, .zip'];
+  const allowedExtensions = ['.pdf', '.jpeg', '.jpg', '.png', '.doc', '.docx', '.txt', '.zip'];
   const extname = path.extname(file.originalname).toLowerCase();
   if (allowedExtensions.includes(extname)) {
     cb(null, true);
@@ -42,16 +41,65 @@ const fileFilter = (req, file, cb) => {
 const upload = multer({ storage: s3Storage, fileFilter: fileFilter });
 
 app.post('/v1/upload-document', upload.single('file'), async (req, res) => {
- 
   if (!req.file) {
     return res.status(400).send('No file uploaded.');
   }
 
-
-
   const uploadedFileUrl = `https://${process.env.AWS_BUCKET_NAME}.s3.amazonaws.com/${req.file.key}`;
-
   res.send('File uploaded successfully');
 });
 
+// update the document name
+app.put('/v1/update-document/:key', async (req, res) => {
+    const { key } = req.params;
+    const { newDocumentName } = req.body;
+  
+    if (!newDocumentName) {
+      return res.status(400).json({ error: "New document name is required." });
+    }
+  
+    const originalFileExtension = path.extname(key);
+    const newKey = newDocumentName + originalFileExtension;
+  
+    try {
+      const copyObjectParams = {
+        CopySource: `${process.env.AWS_BUCKET_NAME}/${key}`,
+        Bucket: process.env.AWS_BUCKET_NAME,
+        Key: newKey,
+      };
+      await s3Client.send(new CopyObjectCommand(copyObjectParams));
+  
+      const deleteObjectParams = {
+        Bucket: process.env.AWS_BUCKET_NAME,
+        Key: key,
+      };
+      await s3Client.send(new DeleteObjectCommand(deleteObjectParams));
+  
+      res.status(200).json({ message: "Document updated successfully." });
+    } catch (error) {
+      console.error("Error updating document:", error);
+      res.status(400).json({ error: "Failed to update the document." });
+    }
+  });
+  
+
+//  delete the document
+app.delete('/v1/delete-document/:key', async (req, res) => {
+  const { key } = req.params;
+
+  try {
+    const deleteObjectParams = {
+      Bucket: process.env.AWS_BUCKET_NAME,
+      Key: key,
+    };
+    await s3Client.send(new DeleteObjectCommand(deleteObjectParams));
+
+    res.status(200).json({ message: "Document deleted successfully." });
+  } catch (error) {
+    console.error("Error deleting document:", error);
+    res.status(400).json({ error: "Failed to delete the document." });
+  }
+});
+
 module.exports = app;
+
