41cf7e2cb9f9dbaced4512394941d5d6c2dfedef
Satish <73182406+Satishchilkaka@users.noreply.github.com>
2023-11-04 19:32:58 -0400

    update get documents endpoint

--- src/routes/getDocuments.js
+++ src/routes/getDocuments.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const { S3Client, ListObjectsCommand } = require("@aws-sdk/client-s3");
+const { MongoClient } = require('mongodb');
 const app = express();
+
+const uri = process.env.MONGODB_URI;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const s3Client = new S3Client({
   region: process.env.AWS_REGION,
@@ -10,31 +14,29 @@ const s3Client = new S3Client({
   },
 });
 
-app.get("/v1/get-documents", async (req, res) => {
-  const params = {
-    Bucket: process.env.AWS_BUCKET_NAME,
-  };
 
-  try {
-    const listObjectsCommand = new ListObjectsCommand(params);
-    const data = await s3Client.send(listObjectsCommand);
+app.get('/v1/get-documents', async (req, res) => {
+    try {
+        // Fetch document metadata from MongoDB.
+        const db = client.db('users_documents');
+        const productsCollection = db.collection('documents');
+        const documents = await productsCollection.find({}).toArray();
 
-    const documents = data.Contents.map((object) => {
-      const { Key, LastModified } = object;
-      const documentUrl = `https://${params.Bucket}.s3.amazonaws.com/${Key}`;
+        // Map documents to include both MongoDB metadata and AWS S3 image URLs.
+        const documentsWithUrls = documents.map((document) => ({
+            name: document.name,
+            category: document.category,
+            imageURL: document.imageURL,
+            uploadDate: document.uploadDate, 
+            id: document._id
 
-      return {
-        name: Key,
-        uploadDate: LastModified,
-        url: documentUrl,
-      };
-    });
+        }));
 
-    res.json(documents);
-  } catch (error) {
-    console.error("Error fetching documents:", error);
-    res.status(500).json({ error: "Failed to fetch documents" });
-  }
+        res.json(documentsWithUrls);
+    } catch (error) {
+        console.error('Error fetching document metadata from MongoDB:', error);
+        res.status(500).json({ error: 'Failed to fetch documents' });
+    }
 });
 
 module.exports = app;
